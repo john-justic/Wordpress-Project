ce9d0f21216919a4c63f63b86f273a8bb3664732
John <johnjustice766@gmail.com>
2023-12-07 23:56:33 +0300

    done

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ document.addEventListener('DOMContentLoaded', function() {
         // Additional styles as needed
     }
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const contactForm = document.getElementById('contact-formm');
+    const sendMessageBtn = document.getElementById('send-message-btn');
+    const messageStatus = document.getElementById('contact-message-status');
+
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        // Simulate sending the message (Dummy functionality)
+        setTimeout(function() {
+            // Display success message after 2 seconds (simulating a successful send)
+            messageStatus.textContent = 'Message sent successfully!';
+            contactForm.reset(); // Clear the form fields
+        }, 2000); // Adjust timing as needed for simulation
+    });
+});
