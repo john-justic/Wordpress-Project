8320218efd9b56d7a14b8498b90af2e26b45159f
John <johnjustice766@gmail.com>
2023-12-07 23:27:30 +0300

    done

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
         userNameElement.textContent = `Welcome, ${signedUpUserName}!`;
         userNameElement.classList.add('user-name');
         userInfo.appendChild(userNameElement);
+
+        // Move the user's name to the top right corner
+        userNameElement.style.position = 'absolute';
+        userNameElement.style.top = '10px';
+        userNameElement.style.right = '10px';
+        userNameElement.style.color = '#fff'; // Example: Change text color to white
+        userNameElement.style.fontWeight = 'bold'; // Example: Make the text bold
+        // Additional styles as needed
     }
 });
